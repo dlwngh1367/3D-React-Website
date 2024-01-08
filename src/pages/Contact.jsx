af7a30fece2bd3d90119886923e013874d3ad52e
Juho Lee <dlwngh1367@gmail.com>
2024-01-07 22:21:18 -0500

    fox 3d model added with lights

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useRef } from 'react'
 import emailjs from "@emailjs/browser";
+import Fox from '../models/Fox'
+import Loader from '../components/Loader'
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from 'react';
+
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -102,7 +107,35 @@ const Contact = () => {
           </button>
         </form>
       </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+      <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
 
+          <Suspense fallback={<Loader />}>
+            <Fox
+             
+              position={[0.5, 0.35, 0]}
+              rotation={[12.629, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
 
     </section>
   )
